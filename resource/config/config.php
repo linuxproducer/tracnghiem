f8cae7af33433e84cfc8dd08bbcfa9115704832b
linuxproducer <minhduchbdl@gmail.com>
2019-09-08 12:48:48 +0700

    upload srouce

---
+++ resource/config/config.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * HỆ THỐNG TRẮC NGHIỆM ONLINE
+ * WEBSITE CONFIG
+ * @author: Nong Van Du (Dzu)
+ * Mail: dzu6996@gmail.com
+ * @link https://github.com/meesudzu/trac-nghiem-online
+ **/
+
+class Config
+{
+    const TITLE = 'Hệ Thống Trắc Nghiệm Online';
+    const COPYRIGHT = "Copyright © 2016 Nong Van Du";
+    const VERSION = "3.4.2 #8";
+    const OWNER = "Nông Văn Du (Dzu)";
+    const CONTRIBUTOR = "";
+    const EMAIL = "dzu6996@gmail.com";
+    const RELEASE = "27/03/2019";
+    const TIMEZONE = "Asia/Bangkok";
+    const APP_URL = "http://localhost/";
+}
